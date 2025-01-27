7dd9f5e5937440015fd92f85ebc90356e83a743d
annaboris-va <anna.boris_va@bk.ru>
2025-01-27 20:49:56 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -18,23 +18,15 @@ limit 10
 ;
 
 --Второй отчет содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.
-with tab as
-(select
+select
 	concat (e.first_name, ' ', e.last_name) as seller, --имя и фамилия продавца
-	count (s.sales_id) as operations, --количество проведенных сделок
-	sum (p.price*s.quantity) as income--суммарная выручка продавца за все время
+	floor (avg (p.price*s.quantity)) as average_income
 from sales as s
 join employees as e on e.employee_id = s.sales_person_id 
 join products as p on p.product_id = s.product_id 
 group by seller
-order by income desc
-)
-select
-	seller,
-	floor (income/operations) as average_income
-from tab
-where floor (income/operations) < (select avg (income/operations) from tab)
-order by average_income ASC
+having avg (p.price*s.quantity) < (select avg (p.price*s.quantity) from sales as s join products as p on p.product_id = s.product_id)
+order by average_income asc
 ;
 
 --Третий отчет содержит информацию о выручке по дням недели для каждого продавца. 
@@ -54,22 +46,14 @@ order by extract (isodow  from s.sale_date), seller
 --Шаг 6. Анализ покупателей.
 --Первый отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
 select 
-	'16-25' as age_category,
+	case 
+		when age >= 16 and age <=25 then '16-25'
+		when age >= 26 and age <=40 then '26-40'
+		when age >= 41 then '40+'
+	end as age_category,
 	count (*) as age_count
 from customers
-where age >= 16 and age <=25
-union 
-select 
-	'26-40' as age_category,
-	count (*) as age_count
-from customers
-where age >= 26 and age <=40
-union 
-select 
-	'40+' as age_category,
-	count (*) as age_count
-from customers
-where age >= 41
+group by age_category
 order by age_category
 ;
 
@@ -90,27 +74,20 @@ with tab as (
 		s.customer_id,
 		concat (c.first_name, ' ', c.last_name) as customer,
 		s.sale_date,
-		concat (e.first_name, ' ', e.last_name) as seller
+		concat (e.first_name, ' ', e.last_name) as seller,
+		row_number () over (partition by concat (c.first_name, ' ', c.last_name), DATE_TRUNC('year', sale_date) order by s.sale_date) as rn
 	from sales as s
 	join employees as e on e.employee_id = s.sales_person_id 
 	join products as p on p.product_id = s.product_id 
 	join customers as c on c.customer_id = s.customer_id
 	where price = 0
 	group by s.customer_id, customer, seller, s.sale_date
-	order by s.customer_id, s.sale_date),
-	rn_tab as (
-	SELECT
-        customer_id,
-		customer,
-		sale_date,
-		seller,
-		row_number () over (partition by customer, DATE_TRUNC('year', sale_date) order by sale_date) as rn
-	FROM tab)
+	order by s.customer_id, s.sale_date)
 select 
 	customer,
 	sale_date,
 	seller
-from rn_tab
+from tab
 where rn = 1
 order by customer_id
 ;
